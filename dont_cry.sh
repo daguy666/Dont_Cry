b64432dd00243884819239a2dfa60b23a943b8dd
Joey Pistone <jpistonejr@gmail.com>
2019-10-21 08:39:03 -0400

    Updated script, switched from older back ticks to dollar sign parentheses.

--- dont_cry.sh
+++ dont_cry.sh
@@ -48,35 +48,34 @@ echo "$PLUS Running Petya/WannaCry Vuln scan against list ..."
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # This is where the magic happens
-$NM $ARGS > $FILE_NAME
+"$NM" "$ARGS" > "$FILE_NAME"
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Gathers some information about the scan and
 # returns it to the command line
-TAIL=`tail -n 1 $FILE_NAME`
-echo $PLUS $TAIL
+TAIL=$(tail -n 1 "$FILE_NAME")
+echo "$PLUS $TAIL"
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Store the output of the following commands to the following variables.
-GR=`grep -i "State: VULNERABLE" $FILE_NAME`
-GR2=`grep -i "State: VULNERABLE" $FILE_NAME | wc -l`
+GR=$(grep -i "State: VULNERABLE" "$FILE_NAME")
+GR2=$(grep -i "State: VULNERABLE" "$FILE_NAME" | wc -l)
 # ready for this hack? This grabs the output and the 10 lines above
 # then parses out the ip address.
-GR3=`grep -i -B 10 "state: VULNERABLE" $FILE_NAME| grep -E $IPREG | awk -F \( '{print $2}' | sed 's/)//g'`
+GR3=$(grep -i -B 10 "state: VULNERABLE" "$FILE_NAME"| grep -E "$IPREG" | awk -F \( '{print $2}' | sed 's/)//g')
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Some logic to deterime if the output of GR is empty or not
 # empty = not vulnerable
 # not empty = vulnerable
 
-if [[ $GR ]]; then
-    echo "$PLUS Systems Vulnerable:" $GR2 "IP Address:" $GR3
+if [[ "$GR" ]]; then
+    echo "$PLUS" "Systems Vulnerable:" "$GR2" "IP Address:" "$GR3"
     # If you don't want to email the results comment out the next line.                                                                         
-    echo -e "\$PLUS $TAIL\n\n$PLUS Systems Vulnerable:" $GR2 "IP Address:" $GR3 | mail -s "WannaCry Vulnerability Report " $EMAIL_RECIPIENTS
+    echo -e "\$PLUS $TAIL\n\n$PLUS Systems Vulnerable:" "$GR2" "IP Address:" "$GR3" | mail -s "WannaCry Vulnerability Report " $EMAIL_RECIPIENTS
 else
     echo "$PLUS No systems vulnerable"
     # If you want emails every time this thing runs uncomment out the next line.
     #echo -e "$PLUS $TAIL\n\n$PLUS no systems vulnerable" | mail -s "WannaCry Vulnerability Report " $EMAIL_RECIPIENTS
 
 fi
-
